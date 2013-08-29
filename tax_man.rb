bf2d7bf4ae0ba0cf2a745254158cca642bbc6ea0
Doug Gerber <dojoh@outlook.com>
2013-08-29 07:38:02 -0400

    edit tax

---
+++ tax_man.rb
@@ -0,0 +1,22 @@
+[
+  {
+    first_name: 'Johnny'
+    last_name: 'Smith',
+    annual_income: 120000
+  },
+  {
+    first_name: 'Liz'
+    last_name: 'Lemon'
+    annual_income: 95000
+  },
+  {
+    first_name: 'Jane',
+    last_name: 'Doe',
+    annual_income: 140000
+  },
+  {
+    first_name: 'Mike',
+    last_name: 'Orsillio'
+    annual_income: 40000
+  }
+]
