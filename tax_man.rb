f08d4af1c825c3c35db211bb5832fb32139df35f
Doug Gerber <dojoh@outlook.com>
2013-08-29 08:00:40 -0400

    Update Taxes

--- tax_man.rb
+++ tax_man.rb
@@ -1,12 +1,14 @@
-[
+# Calculate a tax rate of 22% for each citizen
+
+records = [
   {
-    first_name: 'Johnny'
+    first_name: 'Johnny',
     last_name: 'Smith',
     annual_income: 120000
   },
   {
-    first_name: 'Liz'
-    last_name: 'Lemon'
+    first_name: 'Liz',
+    last_name: 'Lemon',
     annual_income: 95000
   },
   {
@@ -16,7 +18,21 @@
   },
   {
     first_name: 'Mike',
-    last_name: 'Orsillio'
+    last_name: 'Orsillio',
     annual_income: 40000
   }
 ]
+
+# possible variable?
+# tax = 0
+
+# iterate through the records
+
+# output tax owed
+records.each do |name, tax|
+
+  end
+ 
+#loop through the list of records
+  # calculate the effective tax for a specific citizen
+  # output the citizen's tax liability
