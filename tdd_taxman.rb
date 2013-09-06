7f1415780a6c085d3b880cf9ded360fc6cec0a6a
Doug Gerber <dojoh@outlook.com>
2013-09-06 16:28:21 -0400

    TDD Taxman

--- tdd_taxman.rb
+++ tdd_taxman.rb
@@ -1,5 +1,31 @@
+person = [
+  {
+    first_name: 'Johnny',
+    last_name: 'Smith',
+    annual_income: 120000
+  },
+  {
+    first_name: 'Liz',
+    last_name: 'Lemon',
+    annual_income: 95000
+  },
+  {
+    first_name: 'Jane',
+    last_name: 'Doe',
+    annual_income: 140000
+  },
+  {
+    first_name: 'Mike',
+    last_name: 'Orsillio',
+    annual_income: 40000
+  }
+]
+
 class Liability
-  def initialize(income)
+  attr_reader :person
+
+  def initialize(income, person)
+    @person = person
     @income = income
   end
 
@@ -9,5 +35,14 @@ class Liability
     else
       440
     end
+  end
+
+  def taxes
+    @person.each do |person|
+      if person[:first_name] == "Johnny"
+        @answer = person[:annual_income] * 0.22
+      end
+    end
+    @answer.to_i
   end
 end
--- tdd_taxman_spec.rb
+++ tdd_taxman_spec.rb
@@ -1,14 +1,53 @@
 require 'rspec'
 require_relative 'tdd_taxman'
 
+person = [
+  {
+    first_name: 'Johnny',
+    last_name: 'Smith',
+    annual_income: 120000
+  },
+  {
+    first_name: 'Liz',
+    last_name: 'Lemon',
+    annual_income: 95000
+  },
+  {
+    first_name: 'Jane',
+    last_name: 'Doe',
+    annual_income: 140000
+  },
+  {
+    first_name: 'Mike',
+    last_name: 'Orsillio',
+    annual_income: 40000
+  }
+]
+
+
 describe Liability do
 	
 	it 'should calculate tax liability for citizen' do
-		expect(Liability.new(1000).calculate).to eql(220)
+		expect(Liability.new(1000, person).calculate).to eql(220)
 	end
 
 	it 'should calculate tax liability for citizen' do
-		expect(Liability.new(2000).calculate).to eql(440)
+		expect(Liability.new(2000, person).calculate).to eql(440)
 	end
+
+	it 'should citizen pay more or receive a refund' do
+		expect(Liability.new(1000, person).calculate).to eql(220)
+	end
+
+	it 'calling person method will return input hash' do
+		expect(Liability.new(1000, person).person).to eql(person)
+	end
+
+	it 'will output the tax liability of two people' do
+		expect(Liability.new(1000, person).taxes).to eql(26400)
+	end
+
+	it "will ouput the tax liability for all citizens" do
+		expect(Liability.new())
 end
 
