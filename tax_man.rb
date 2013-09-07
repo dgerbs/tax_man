72f846614416d5cbc0bc11a1b11fd144324641db
Doug Gerber <dojoh@outlook.com>
2013-09-07 16:00:02 -0400

    Final Tax Eval

--- tax_man.rb
+++ tax_man.rb
@@ -1,45 +1,5 @@
 # #!/usr/bin/env ruby
 
-# records = [
-#   {
-#     first_name: 'Johnny',
-#     last_name: 'Smith',
-#     annual_income: 120000
-#   },
-#   {
-#     first_name: 'Liz',
-#     last_name: 'Lemon',
-#     annual_income: 95000
-#   },
-#   {
-#     first_name: 'Jane',
-#     last_name: 'Doe',
-#     annual_income: 140000
-#   },
-#   {
-#     first_name: 'Mike',
-#     last_name: 'Orsillio',
-#     annual_income: 40000
-#   }
-# ]
-
-# # possible variable?
-# # tax = 0
-
-# # iterate through the records
-
-# TAX_RATE = 0.22
-
-# records.each do |record|
-#   efffective_tax = TAX_RATE * record[:annual_income]
-
-#   puts "#{ [record:[:first_name, :last_name]].join(" ")}"
-#   end
-# Calculate a tax rate of 22% for each citizen
-
-# Output tax owed
-
-
 records = [
   {
     first_name: 'Johnny',
@@ -66,11 +26,26 @@ records = [
     tax_paid: 8800
   }
 ]
+ 
+TAX_RATE = 0.22
+ 
+2.times do
+  records.each do |record|
+    #calculate the tax liability based on a flat tax
+    tax = TAX_RATE * record[:annual_income]
 
-records.each do |record|
-  efffective_tax = TAX_RATE * record[:annual_income]
-  refund = record[:tax_paid] - efffective_tax
-  if refund < 0
+    refund = record[:tax_paid] - tax
 
-  puts "#{ [record:[:first_name, :last_name]].join(" ")}"
+    join = [record[:first_name], record[:last_name]].join(" ")
+    output = ""
+    # determine if citizen owes tax
+    if refund < 0
+      # if true: output amount owed
+      output = "owes #{refund * -1}"
+    else
+      # if false: output how much they get back
+      output = "will receive a refund of #{refund}"
+    end
+    puts join + " " + output
   end
+end
